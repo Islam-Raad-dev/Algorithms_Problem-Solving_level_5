4acf7880bdd31d798a5597ea38a0be2ff509249b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-06 14:16:27 +0300

    Edit : 34

--- Project_3/clsMyStack.h
+++ Project_3/clsMyStack.h
@@ -1,10 +1,39 @@
 #pragma once
 
 #include <iostream>
+#include "ClsMyDblLinkedList.h"
 using namespace std;
 
 template <class T>
 
 class clsMyStack
 {
+
+protected:
+    clsDblLinkeList<T> _MyList;
+
+public:
+    void push(T Value)
+    {
+    }
+
+    void pop()
+    {
+    }
+
+    void print()
+    {
+    }
+
+    int size()
+    {
+    }
+
+    T Top()
+    {
+    }
+
+    T Bottom()
+    {
+    }
 };
