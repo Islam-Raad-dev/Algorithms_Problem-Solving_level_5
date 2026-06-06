8ff30cd61b776b7d59f12584b8e2c14478ff58d4
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-06 12:14:15 +0300

    Add File 2

---
+++ Project_2/ClsMyDblLinkedList.h
@@ -0,0 +1,288 @@
+/*
+
+Double Linked List Class: 
+
+ A double linked list is a data structure that consists of a sequence of 
+ nodes, where each node contains a value and two pointers: one pointing to 
+ the next node in the sequence and another pointing to the previous node. 
+ This allows for efficient insertion and deletion of nodes at both ends of 
+ the list, as well as traversal in both directions.
+
+*/
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class clsDblLinkeList
+{
+
+protected:
+    int _Size = 0;
+
+public:
+    class Node
+    {
+    public:
+        T Value;
+        Node *Next;
+        Node *Prev;
+    };
+
+    Node *Head = NULL;
+
+    void InsertAtBeginning(T Value)
+    {
+        Node *NewNode = new Node();
+        NewNode->Value = Value;
+        NewNode->Next = Head;
+        NewNode->Prev = NULL;
+
+        if (Head != NULL)
+        {
+            Head->Prev = NewNode;
+        }
+
+        Head = NewNode;
+        _Size++;
+    }
+
+    void PrintList()
+    {
+        Node *Current = Head;
+
+        while (Current != NULL)
+        {
+            cout << Current->Value << " ";
+            Current = Current->Next;
+        }
+
+        cout << endl;
+    }
+
+    Node *Find(T Value)
+    {
+        Node *Current = Head;
+
+        while (Current != NULL)
+        {
+            if (Current->Value == Value)
+            {
+                return Current;
+            }
+            Current = Current->Next;
+        }
+        return NULL;
+    }
+
+    void InsertAfter(Node *Current, T Value)
+    {
+        Node *NewNode = new Node();
+        NewNode->Value = Value;
+        NewNode->Next = Current->Next;
+        NewNode->Prev = Current;
+
+        if (Current->Next != NULL)
+        {
+            Current->Next->Prev = NewNode;
+        }
+
+        Current->Next = NewNode;
+        _Size++;
+    }
+
+    void InsertAtEnd(T Value)
+    {
+
+        Node *NewNode = new Node();
+        NewNode->Value = Value;
+        NewNode->Next = NULL;
+
+        if (Head == NULL)
+        {
+            NewNode->Prev = NULL;
+            Head = NewNode;
+        }
+        else
+        {
+            Node *Current = Head;
+            while (Current->Next != NULL)
+            {
+                Current = Current->Next;
+            }
+
+            Current->Next = NewNode;
+            NewNode->Prev = Current;
+        }
+        _Size++;
+    }
+
+    void DeleteNode(Node *&NodeToDelete)
+    {
+        if (Head == NULL || NodeToDelete == NULL)
+        {
+            return;
+        }
+
+        if (Head == NodeToDelete)
+        {
+            Head = NodeToDelete->Next;
+        }
+
+        if (NodeToDelete->Next != NULL)
+        {
+            NodeToDelete->Next->Prev = NodeToDelete->Prev;
+        }
+
+        if (NodeToDelete->Prev != NULL)
+        {
+            NodeToDelete->Prev->Next = NodeToDelete->Next;
+        }
+
+        delete NodeToDelete;
+        _Size--;
+    }
+
+    void DeleteFirstNode()
+    {
+        if (Head == NULL)
+        {
+            return;
+        }
+
+        Node *Temp = Head;
+        Head = Head->Next;
+
+        if (Head != NULL)
+        {
+            Head->Prev = NULL;
+        }
+
+        delete Temp;
+        _Size--;
+    }
+
+    void DeleteLastNode()
+    {
+        if (Head == NULL)
+        {
+            return;
+        }
+
+        Node *Current = Head;
+        while (Current->Next != NULL)
+        {
+            Current = Current->Next;
+        }
+
+        if (Current->Prev != NULL)
+        {
+            Current->Prev->Next = NULL;
+        }
+        else
+        {
+            Head = NULL;
+        }
+
+        delete Current;
+        _Size--;
+    }
+
+    int size()
+    {
+        return _Size;
+    }
+
+    bool IsEmpty()
+    {
+        return _Size == 0 ? true : false;
+    }
+
+    void clear()
+    {
+        while (_Size > 0)
+        {
+            DeleteFirstNode();
+        }
+    }
+
+    void Reverse()
+    {
+        Node *Current = Head;
+        Node *Temp = nullptr;
+
+        while (Current != nullptr)
+        {
+            Temp = Current->Prev;
+            Current->Prev = Current->Next;
+            Current->Next = Temp;
+            Current = Current->Prev;
+        }
+
+        if (Temp != nullptr)
+        {
+            Head = Temp->Prev;
+        }
+    }
+
+    Node *GetNode(int Index)
+    {
+        int Counter = 0;
+
+        if (Index < 0 || Index >= _Size)
+        {
+            return NULL;
+        }
+
+        Node *Current = Head;
+
+        while (Current != NULL && Current->Next != NULL)
+        {
+            if (Counter == Index)
+            {
+                break;
+            }
+            Current = Current->Next;
+            Counter++;
+        }
+
+        return Current;
+    }
+
+    T GetItem(int Index)
+    {
+        Node *ItemNode = GetNode(Index);
+
+        if (ItemNode == NULL)
+            return T();
+        else
+            return ItemNode->Value;
+    }
+
+    bool UpdateItem(int Index, T NewValue)
+    {
+        Node *ItemNode = GetNode(Index);
+
+        if (ItemNode != NULL)
+        {
+            ItemNode->Value = NewValue;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool InsertAfter(int Index, T Value)
+    {
+        Node *ItemNode = GetNode(Index);
+
+        if (ItemNode != NULL)
+        {
+            InsertAfter(ItemNode, Value);
+            return true;
+        }
+        else
+            return false;
+    }
+};
