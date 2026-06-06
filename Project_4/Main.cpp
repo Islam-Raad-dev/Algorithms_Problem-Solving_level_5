be753467348e34f54e9f1ae3c9789f89096588a0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-06 15:35:03 +0300

    Edit : 55

--- Project_4/Main.cpp
+++ Project_4/Main.cpp
@@ -11,6 +11,17 @@ using namespace std;
 
 int main()
 {
+    clsMyDynaminArray <int> MyDynaminArray(5);
+
+    MyDynaminArray.SetItem(0, 1);
+    MyDynaminArray.SetItem(0, 2);
+    MyDynaminArray.SetItem(0, 3);
+    MyDynaminArray.SetItem(0, 4);
+    MyDynaminArray.SetItem(0, 5);
+
+    MyDynaminArray.PrintList();
+
+    cout << "Is Empty : " ; MyDynaminArray.IsEmpty();
 
     return 0;
 }
