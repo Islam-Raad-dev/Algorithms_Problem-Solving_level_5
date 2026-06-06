f1b334bba75929f3ee6045c339442e8d5bdeb467
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-06 14:42:01 +0300

    My Stack Project

--- Project_3/Main.cpp
+++ Project_3/Main.cpp
@@ -10,5 +10,45 @@ using namespace std;
 
 int main()
 {
+    clsMyStack <int> MyStack;
+
+    MyStack.push(10);
+    MyStack.push(20);
+    MyStack.push(30);
+    MyStack.push(40);
+    MyStack.push(50);
+
+
+    cout << "\nStack: \n";
+    MyStack.Print();
+
+    cout << "\nStack Size: " << MyStack.size();
+    cout << "\nStack Top: " << MyStack.Top();
+    cout << "\nStack Bottom: " << MyStack.Bottom();
+
+    MyStack.pop();
+
+    cout << "\n\nStack after pop() : \n";
+    MyStack.Print();
+
+    cout << "\n\n Item(2) : " << MyStack.GetItem(2);
+    
+    MyStack.Reverse();
+    cout << "\n\nStack after reverse() : \n";
+    MyStack.Print();
+
+    MyStack.UpdateItem(2, 600);
+    cout << "\n\nStack after updating Item(2) to 600 : \n";
+    MyStack.Print();
+
+    MyStack.InsertAfter(2, 800);
+    cout << "\n\nStack after Inserting 800 after Item(2) : \n";
+    MyStack.Print();
+
+
+    MyStack.clear();
+    cout << "\n\nStack after Clear(): \n";
+    MyStack.Print();
+
     return 0;
 }
