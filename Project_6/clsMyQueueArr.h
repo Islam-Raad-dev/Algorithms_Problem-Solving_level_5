8b6007fb22c2956fe7468c752bdab83ec57b0dc9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-07 15:12:51 +0300

    Add Files: 8

---
+++ Project_6/clsMyQueueArr.h
@@ -0,0 +1,91 @@
+/*
+
+Queue Array Class
+
+*/
+#pragma once
+
+#include <iostream>
+#include "clsMYDynamicArray.h"
+
+using namespace std;
+
+template <class T>
+
+class clsMyQueueArr
+{
+
+protected:
+    clsMyDynaminArray < T > _MyList;
+
+public:
+    void push(T Item)
+    {
+        _MyList.InsertAtEnd(Item);
+    }
+
+    void pop()
+    {
+        _MyList.DeleteFirstItem();
+    }
+
+    T front()
+    {
+        return _MyList.GetItem(0);
+    }
+
+    T back()
+    {
+        return _MyList.GetItem(size() - 1);
+    }
+
+    int size()
+    {
+        return _MyList.size();
+    }
+
+    bool IsEmpty()
+    {
+        return _MyList.IsEmpty();
+    }
+
+    void print()
+    {
+        _MyList.PrintList();
+    }
+
+    void Reverse()
+    {
+        _MyList.Reverse();
+    }
+
+    T GetItem(int Index)
+    {
+        return _MyList.GetItem(Index);
+    }
+
+    void UpdateItem(int Index, T NewValue)
+    {
+        _MyList.UpdateItem(Index, NewValue);
+    }
+
+    void InsertAfter(int Index, T NewValue)
+    {
+        _MyList.InsertAfter(Index, NewValue);
+    }
+
+    void InsetAtFront(T Value)
+    {
+        _MyList.InsertAtBeginning(Value);
+    }
+
+    void InsetAtBack(T Value)
+    {
+        _MyList.InsertAtEnd(Value);
+    }
+
+    void clear()
+    {
+        _MyList.clear();
+    }
+};
