cf01b2c81c6e2cf289570aed7156036cbc7d7502
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-07 15:13:53 +0300

    Stack Array Project

--- Project_6/Main.cpp
+++ Project_6/Main.cpp
@@ -21,42 +21,42 @@ int main()
     MyStack.push(50);
 
     cout << "\nStack: \n";
-    MyStack.Print();
+    MyStack.print();
 
-    cout << "\nStack Size: " << MyStack.Size();
+    cout << "\nStack Size: " << MyStack.size();
     cout << "\nStack Top: " << MyStack.Top();
     cout << "\nStack Bottom: " << MyStack.Bottom();
 
     MyStack.pop();
 
     cout << "\n\nStack after pop() : \n";
-    MyStack.Print();
+    MyStack.print();
 
     cout << "\n\n Item(2) : " << MyStack.GetItem(2);
 
     MyStack.Reverse();
     cout << "\n\nStack after reverse() : \n";
-    MyStack.Print();
+    MyStack.print();
 
     MyStack.UpdateItem(2, 600);
     cout << "\n\nStack after updating Item(2) to 600 : \n";
-    MyStack.Print();
+    MyStack.print();
 
     MyStack.InsertAfter(2, 800);
     cout << "\n\nStack after Inserting 800 after Item(2) : \n";
-    MyStack.Print();
+    MyStack.print();
 
-    MyStack.InsertAtFront(1000);
+    MyStack.InsetAtFront(1000);
     cout << "\n\nStack after Inserting 1000 at top: \n";
-    MyStack.Print();
+    MyStack.print();
 
-    MyStack.InsertAtBack(2000);
+    MyStack.InsetAtBack(2000);
     cout << "\n\nStack after Inserting 2000 at bottom: \n";
-    MyStack.Print();
+    MyStack.print();
 
-    MyStack.Clear();
+    MyStack.clear();
     cout << "\n\nStack after Clear(): \n";
-    MyStack.Print();
+    MyStack.print();
 
     return 0;
 }
