59f2aff3d1e996578f253d1cc64970a82cb6a5f1
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-08 18:51:50 +0300

    Small Project Class

--- Project_7/Main.cpp
+++ Project_7/Main.cpp
@@ -14,43 +14,41 @@ int main()
 
     clsMyString S1;
 
-    cout << "\nS1  = " << S1.Value << "\n";
+    cout << "\nS1  = " << S1.GetValue() << "\n";
 
-    S1.Value = "Mohammed";
+    S1.SetValue("Mohammed");
+    cout << "S1  = " << S1.GetValue() << "\n";
 
-    cout << "S1  = " << S1.Value << "\n";
+    S1.SetValue("Mohammed2");
+    cout << "S1  = " << S1.GetValue() << "\n";
 
-    S1.Value = "Mohammed2";
-
-    cout << "S1  = " << S1.Value << "\n";
-
-    S1.Value = "Mohammed3";
-
-    cout << "S1  = " << S1.Value << "\n";
+    S1.SetValue("Mohammed3");
+    cout << "S1  = " << S1.GetValue() << "\n";
 
     cout << "\n\nUndo: ";
     cout << "\n__________\n";
 
     S1.Undo();
-    cout << "\nS1  after undo = " << S1.Value << "\n";
+    cout << "\nS1  after undo = " << S1.GetValue() << "\n";
 
     S1.Undo();
-    cout << "S1  after undo = " << S1.Value << "\n";
+    cout << "S1  after undo = " << S1.GetValue() << "\n";
 
     S1.Undo();
-    cout << "S1  after undo = " << S1.Value << "\n";
+    cout << "S1  after undo = " << S1.GetValue() << "\n";
 
     cout << "\n\nRedo: ";
     cout << "\n__________\n";
 
     S1.Redo();
-    cout << "\nS1  after Redo = " << S1.Value << "\n";
+    cout << "\nS1  after Redo = " << S1.GetValue() << "\n";
 
     S1.Redo();
-    cout << "S1  after Redo = " << S1.Value << "\n";
+    cout << "S1  after Redo = " << S1.GetValue() << "\n";
 
     S1.Redo();
-    cout << "S1  after Redo = " << S1.Value << "\n";
+    cout << "S1  after Redo = " << S1.GetValue() << "\n";
 
+    system("pause>0");
     return 0;
 }
